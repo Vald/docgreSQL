b76471eeb6172b3743c0c52d3f17065ea4ae8768
Vladislav Navel <valdounet@gmail.com>
2012-03-13 11:15:20 +0100

    Initialisation des classes TablesDoc et TablesList Ça risque de bouger, mais c'est pour commencer

--- lib/table_def.py
+++ lib/table_def.py
@@ -5,8 +5,30 @@ import sys, os, string, re, codecs
 
 
 # TODO : gérer les champs qui peuvent être plusieurs ... (genre field :)  )
-# TODO : ne pas traiter les repertoires et fichiers cachés
 # TODO : créer un .log pour que quand ça plante on puisse suivre ce qui se passe
+
+class TablesList:
+	"""
+	Class to deal with textual description of tables.
+
+	Before string are transformed to valid dictionnary.
+	"""
+	def __init__(self):
+		self.tables={}
+	def addTable(self, table, query):
+		if 'name' in table.keys():
+			if table['name'] in self.tables.keys():
+				raise NameError("table '"+tabe['name']+"' is documented several times.")
+			table['query']=query
+			self.tables[table['name']]=table
+		else:
+			errMsg="""
+			A documenting section is found but without name definition."""
+			if len(table.keys()) > 0:
+				errMsg=errMsg+"""(first field is : """+table.keys()[0]+": "+table.values()[0]+")"
+			raise NameError(errMsg)
+
+
 
 class DataBase:
 	"""
@@ -18,16 +40,24 @@ class DataBase:
 		"""
 		if not os.path.isdir(root):
 			raise NameError(root+" is not defined")
-		# list of all subdirectories
+
+		# list of all subdirectories from root.
+		# TODO : remove hidden subdirectories
+
 		paths = [root+'/'+ x for x in os.listdir(root) if os.path.isdir(root+'/'+x)]
-		# list of all files
+		paths.append(root)
+
+		# list of all files defined in the subtree from root
+		# only sql files are kept
+
 		files = [p+'/'+x for p in paths for x in os.listdir(p)]
+		files = [file for file in files if re.search('\.sql$', file) is not None]
 		
 		# read every tables from every files
-		self.tablesDoc = {}
+
+		self.tablesDoc = TablesList()
 		for file in files:
 			self.tablesDoc = self.readTables(file, self.tablesDoc)
-		# nécessité de vérifier l'unicité des tables ?
 		# interpretaion de la doc
 		# complétion des inheritances
 
@@ -38,39 +68,35 @@ class DataBase:
 		"""
 		if not os.path.exists(file):
 			raise NameError("File '"+file+"' doesn't exist.")
-		in_doc=False		# lors du parcours du fichier, permet de savoir si le curseur
-					# est dans une partie de documentation ou de requete.
-		table={}
-		query=""
-		file=codecs.open(file,'r','utf8')
+		in_doc	= True	# Boolean used to know if the line being read is in a documenting section or not
+		table	= {}	# used to store the table being read
+		query	= ""	# used to store the query definition of the table
+		file	= codecs.open(file,'r','utf8')
 		for line in file:
 			# si on est au début d'une partie de doc on sauve la derniere table lue
 			if re.match("--#", line) is not None and not in_doc:
-				if 'name' in table.keys():
-					table['query']=query
-					tables[table['name']]=table
-				in_doc=True
-				table={}
-				query=""
+				tables.addTable(table, query)
+				in_doc	= True
+				table	= {}
+				query	= ""
 			# si on est au début d'un espace de requete
 			elif re.match("--#", line) is None and in_doc:
-				in_doc=False
+				in_doc	= False
 			# recherche du champ name dans la doc
 			if in_doc:
 				if self.isFirstLineOfField(line):
-					field_name=self.getNameOfField(line)
-					table[field_name]=self.getValueOfField(line)
+					field_name		= self.getNameOfField(line)
+					table[field_name]	= self.getValueOfField(line)
 				else:
-					tmp=table[field_name]
-					table[field_name]=tmp+'\n'+self.getValueOfField(line)
+					
+					table[field_name]	= table[field_name]+'\n'+self.getValueOfField(line)
+
 			# lecture de la requete
 			else:
 				query=query+'\n'+line
-
+				del field_name
 		# integration de la derniere table
-		if 'name' in table.keys():
-			table['query']=query
-			tables[table['name']]=table
+		tables.addTable(table)
 		file.close()
 		return tables
 
@@ -84,9 +110,9 @@ class DataBase:
 
 class TableDoc:
 	"""
-	Classe utilisée pour la représentation des définitions de table\n\
-	\n\
-	--> contient tous les attributs possibles (avec éventuellement des valeurs None) :\n\
+	Classe utilisée pour la représentation des définitions de table
+
+	--> contient tous les attributs possibles (avec éventuellement des valeurs None) :
 	titre, description, dependances, requête"""
 	
 	def __init__(self, titre,description,dependances,champs,requete):
