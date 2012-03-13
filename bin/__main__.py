b3ce37603e9aff9f49251156f1d73924216e9552
Vladislav Navel <valdounet@gmail.com>
2012-03-13 18:07:33 +0100

    update to allow testing

--- bin/__main__.py
+++ bin/__main__.py
@@ -2,9 +2,9 @@
 # encoding: utf-8
 # imports
 import sys, os, string, re, codecs
-sys.path.append("/home/vladislav/src/postdoc/managedoc/")
+sys.path.append("/home/vladislav/src/postdoc/lib/")
 from postdoc import PostDoc
-from table_def import DataBase
+from table_def import *
 
 postdoc=PostDoc("/home/vladislav/src/postdoc/")
 root="/home/vladislav/src/postdoc/test/"
